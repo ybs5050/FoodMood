4a06c8852e264490100f976b0e6b272825cf0e90
Youngmin Son <bfid111@gmail.com>
2017-09-17 14:38:23 -0400

    inital upload

---
+++ src/FoodMood.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ */
+public class FoodMood {
+    
+    private int foodMoodID;
+    private String foodName;
+    
+    /**
+     * This is the default constructor for the FoodMood Class
+     */ 
+    public FoodMood() {
+        
+    }
+    
+    /**
+     * Returns the Food Mood ID for this object
+     * @return A int representing the Food Mood ID for the food mood object
+     */
+    public int getFoodMoodID() {
+        return this.foodMoodID;
+    }
+    
+    /**
+     * Sets a new value for food mood's name
+     * @param newFoodName Sets a new name for the food mood object
+     */
+    public void setFoodName(String newFoodName) {
+        this.foodName = newFoodName;
+    }
+    
+    /**
+     * Returns the object's food name
+     * @return A string represending t he name for the food mood object
+     */
+    public String getFoodName() {
+        return this.foodName;
+    }
+    
+    
+}
---
+++ src/app.java
@@ -0,0 +1,34 @@
+
+import javafx.stage.Stage;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ */
+public class app {
+    
+    
+    /**
+     * Stage method for Main Menu 
+     * @param primaryStage None for now
+     * @throws java.lang.Exception 
+     */
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+    
+    /**
+     * Main method for the execution of this application
+     * @param args None for normal use
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
