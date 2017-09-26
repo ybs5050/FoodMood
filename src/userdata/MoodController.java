a6aa75a80de02e20111df609e64ad5c209b97aed
Youngmin Son <bfid111@gmail.com>
2017-09-26 02:08:39 -0400

    added FXML abstact method stubs

--- src/userdata/MoodController.java
+++ src/userdata/MoodController.java
@@ -5,16 +5,21 @@
  */
 package userdata;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
 /**
  * FXML Controller Class
  * @author jdgra_000
  */
-public class MoodController {
+public class MoodController implements Initializable {
     /**
      * Initializes the controller class
      */
-    public void initialize() {
-        
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
     /**
@@ -23,14 +28,16 @@ public class MoodController {
      * @param moodDescription 
      */
     public void addMood(String moodName, String moodDescription) {
-        
+        System.out.println("Add Mood action performed");
     }
     
     /**
      * Validates text fields
+     * @param textField
+     * @return false if given text is not empty, true if given text is empty
      */
-    public void vaildate() {
-        
+    public boolean vaildate(String textField) {
+        return textField.trim().length() == 0;
     }
-    
+  
 }
