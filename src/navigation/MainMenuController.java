ca16a14bad3201905837999a3086da0b2096e75c
Youngmin Son <bfid111@gmail.com>
2017-09-26 02:07:23 -0400

    changed name from MainMenu.java

---
+++ src/navigation/MainMenuController.java
@@ -0,0 +1,37 @@
+package navigation;
+
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * FXML Controller Class
+ * @author
+ */
+public class MainMenuController implements Initializable {
+    
+    /**
+     * Initializes the controller class
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("MainMenu Window Initialized"); 
+    }
+    
+    /**
+     * Opens a new scene from FoodMoodList class
+     * @param event "View Mood Food" button action
+     */
+    public void viewFoodMoodList(ActionEvent event) {
+        System.out.println(event.getID() + "View Food Mood List event occured");
+    }
+    
+}
