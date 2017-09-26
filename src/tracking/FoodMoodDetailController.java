cf748773a990662084bc1216cbd25be25960f76a
Youngmin Son <bfid111@gmail.com>
2017-09-26 02:07:46 -0400

    added FXML abstract code stub

--- src/tracking/FoodMoodDetailController.java
+++ src/tracking/FoodMoodDetailController.java
@@ -1,5 +1,9 @@
 package tracking;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,20 +14,23 @@ package tracking;
  * FXML Controller Class
  * @author jdgra_000
  */
-public class FoodMoodDetailController {
+public class FoodMoodDetailController implements Initializable {
     
     /**
      * Initializes the controller class
      */
-    public void initialize() {
-        
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("FoodMoodDetail Window Initialized"); 
     }
     
     /**
      * Resets the food mood detail user interface 
      */
     public void reset() {
-        
+        System.out.println("Resetting Window");
     }
+
+    
   
 }
